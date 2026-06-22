083fa901b3c85e2d089b002d0273849113e9500b
“laifa” <zhonglaifa.ohxz@bytedance.com>
2026-06-22 18:28:05 +0800

    feat: 升级fastjson-alibaba组件为fastjson2

--- src/main/java/com/byteplus/rec/sdk/content/example/Main.java
+++ src/main/java/com/byteplus/rec/sdk/content/example/Main.java
@@ -1,6 +1,6 @@
 package com.byteplus.rec.sdk.content.example;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.byteplus.rec.core.BizException;
 import com.byteplus.rec.core.NetException;
 import com.byteplus.rec.core.Option;
--- src/main/java/com/byteplus/rec/sdk/retail/example/entity/DemoUser.java
+++ src/main/java/com/byteplus/rec/sdk/retail/example/entity/DemoUser.java
@@ -1,6 +1,6 @@
 package com.byteplus.rec.sdk.retail.example.entity;
 
-import com.alibaba.fastjson.annotation.JSONField;
+import com.alibaba.fastjson2.annotation.JSONField;
 import lombok.Data;
 
 @Data
